a5e32ae6fd2b3b30b23e3e3fd9c5cefa78e38c68
Kenneth Sese <ksese@hope4tomorrow.net>
2023-04-28 12:11:41 -0600

    WIP

--- resources/views/columns/avatar-group-column.blade.php
+++ resources/views/columns/avatar-group-column.blade.php
@@ -1,32 +1,95 @@
-<div class="filament-tables-avatar-group-column px-4 py-3">
-    <div class="flex items-center space-x-2">
-        <div class="flex -space-x-1">
-            <img
-                class="max-w-none inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-            >
-            <img
-                class="max-w-none inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-            >
-            <img
-                class="max-w-none inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-                alt=""
-            >
-            <img
-                class="max-w-none inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-            >
-            <div class="h-6 w-6 rounded-full ring-2 ring-white bg-gray-300 flex items-center justify-center">
-                <span class="text-xs text-gray-500">+3</span>
+@php
+    $size = $getSize() ?? 'xl';
+    $ring = $getRing() ?? 2;
+
+    $imageClasses = \Illuminate\Support\Arr::toCssClasses([
+        match ($ring) {
+            0 => 'ring-0',
+            1 => 'ring-1',
+            2 => 'ring-2',
+            3 => 'ring-3',
+            4 => 'ring-4',
+            5 => 'ring-5',
+            default => null,
+        },
+        match ($size) {
+            'xs' => 'h-3 w-3',
+            'sm' => 'h-4 w-4',
+            'md' => 'h-5 w-5',
+            'lg' => 'h-6 w-6',
+            'xl' => 'h-7 w-7',
+            '2xl' => 'h-8 w-8',
+            '3xl' => 'h-10 w-10',
+            default => null,
+        },
+    ]);
+    
+    $imageCount = 0;
+@endphp
+
+<div {{ $attributes->merge($getExtraAttributes())->class([
+    'filament-tables-avatar-group-column',
+    'px-4 py-3' => ! $isInline(),
+]) }}>
+    <div 
+    
+    class="flex items-center space-x-2">
+        <div @class([
+            'flex',
+            match ($getOverlap()) {
+                'sm' => '-space-x-1',
+                'md' => '-space-x-2',
+                'lg' => '-space-x-3',
+                'xl' => '-space-x-4',
+                default => '-space-x-1',
+            },
+        ])>
+            @foreach (array_slice($getImages(), 0, $getLimit()) as $image)
+                @if ($path = $getImagePath($image))
+                    @php
+                        $imageCount ++;
+                    @endphp
+                    <img
+                        src="{{ $path }}"
+                        {{ $getExtraImgAttributeBag()->class([
+                            'max-w-none rounded-full ring-white rounded-full dark:ring-gray-800',
+                            $imageClasses => $imageClasses,
+                        ]) }}
+                    >
+                @endif
+            @endforeach
+
+            @if ($shouldShowRemaining() && ! $shouldShowRemainingAsText() && ($imageCount < count($getImages())))
+                <div @class([
+                    'flex items-center justify-center bg-gray-100 text-gray-500 ring-white rounded-full dark:bg-gray-600 dark:text-gray-300 dark:ring-gray-800',
+                    $imageClasses => $imageClasses,
+                    match ($getRemainingTextSize()) {
+                        'xs' => 'text-xs',
+                        'sm' => 'text-sm',
+                        'md' => 'text-md',
+                        'lg' => 'text-lg',
+                        default => 'text-sm',
+                    },
+                ])>
+                    +{{ count($getImages()) - $imageCount }}
+                </div>
+            @endif
+
+        </div>
+
+        @if ($shouldShowRemaining() && $shouldShowRemainingAsText() && ($imageCount < count($getImages())))
+            <div @class([
+                'text-gray-500 dark:text-gray-300',
+                match ($getRemainingTextSize()) {
+                    'xs' => 'text-xs',
+                    'sm' => 'text-sm',
+                    'md' => 'text-md',
+                    'lg' => 'text-lg',
+                    default => 'text-sm',
+                },
+            ])>
+                +{{ count($getImages()) - $imageCount }}
             </div>
-        </div>
-        <div class="text-xs text-gray-500">
-            +3
-        </div>
+        @endif
     </div>
 </div>
--- src/Columns/AvatarGroupColumn.php
+++ src/Columns/AvatarGroupColumn.php
@@ -2,9 +2,244 @@
 
 namespace Archilex\AvatarGroupColumn\Columns;
 
+use Closure;
 use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\Concerns\HasSize;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\ComponentAttributeBag;
+use League\Flysystem\UnableToCheckFileExistence;
+use Throwable;
 
 class AvatarGroupColumn extends Column
 {
+    use HasSize;
+
     protected string $view = 'filament-avatar-group-column::columns.avatar-group-column';
+    
+    protected string | Closure | null $disk = null;
+    
+    protected string | Closure $visibility = 'public';
+    
+    protected string | Closure | null $separator = null;
+    
+    protected int | Closure | null $limit = null;
+    
+    protected int | Closure | null $ring = null;
+
+    protected string | Closure | null $overlap = null;
+
+    protected bool | Closure $shouldShowRemaining = false;
+
+    protected bool | Closure $shouldShowRemainingAsText = false;
+
+    protected string | Closure | null $remainingTextSize = null;
+
+    protected array $extraImgAttributes = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->disk(config('tables.default_filesystem_disk'));
+    }
+
+    public function disk(string | Closure | null $disk): static
+    {
+        $this->disk = $disk;
+
+        return $this;
+    }
+
+    public function visibility(string | Closure $visibility): static
+    {
+        $this->visibility = $visibility;
+
+        return $this;
+    }
+
+    public function getDisk(): Filesystem
+    {
+        return Storage::disk($this->getDiskName());
+    }
+
+    public function getDiskName(): string
+    {
+        return $this->evaluate($this->disk) ?? config('tables.default_filesystem_disk');
+    }
+    
+    public function getImagePath($image): ?string
+    {
+        if (! $image) {
+            return null;
+        }
+
+        if (filter_var($image, FILTER_VALIDATE_URL) !== false) {
+            return $image;
+        }
+
+        /** @var FilesystemAdapter $storage */
+        $storage = $this->getDisk();
+
+        try {
+            if (! $storage->exists($image)) {
+                return null;
+            }
+        } catch (UnableToCheckFileExistence $exception) {
+            return null;
+        }
+
+        if ($this->getVisibility() === 'private') {
+            try {
+                return $temporaryImage = $storage->temporaryUrl(
+                    $image,
+                    now()->addMinutes(5),
+                );
+            } catch (Throwable $exception) {
+                // This driver does not support creating temporary URLs.
+            }
+        }
+
+        return $storage->url($image);
+    }
+
+    public function getVisibility(): string
+    {
+        return $this->evaluate($this->visibility);
+    }
+
+    public function ring(string | Closure | null $ring): static
+    {
+        $this->ring = $ring;
+
+        return $this;
+    }
+
+    public function getRing(): ?int
+    {
+        return $this->evaluate($this->ring);
+    }
+
+    public function overlap(string | Closure | null $overlap): static
+    {
+        $this->overlap = $overlap;
+
+        return $this;
+    }
+
+    public function getOverlap(): ?string
+    {
+        return $this->evaluate($this->overlap);
+    }
+
+    public function extraImgAttributes(array | Closure $attributes, bool $merge = false): static
+    {
+        if ($merge) {
+            $this->extraImgAttributes[] = $attributes;
+        } else {
+            $this->extraImgAttributes = [$attributes];
+        }
+
+        return $this;
+    }
+
+    public function getExtraImgAttributes(): array
+    {
+        $temporaryAttributeBag = new ComponentAttributeBag();
+
+        foreach ($this->extraImgAttributes as $extraImgAttributes) {
+            $temporaryAttributeBag = $temporaryAttributeBag->merge($this->evaluate($extraImgAttributes));
+        }
+
+        return $temporaryAttributeBag->getAttributes();
+    }
+
+    public function getExtraImgAttributeBag(): ComponentAttributeBag
+    {
+        return new ComponentAttributeBag($this->getExtraImgAttributes());
+    }
+
+    public function getImages(): array
+    {
+        $images = $this->getState();
+
+        if (is_array($images)) {
+            return $images;
+        }
+
+        if (! ($separator = $this->getSeparator())) {
+            return [];
+        }
+
+        $images = explode($separator, $images);
+
+        if (count($images) === 1 && blank($images[0])) {
+            $images = [];
+        }
+
+        return $images;
+    }
+
+    public function separator(string | Closure | null $separator = ','): static
+    {
+        $this->separator = $separator;
+
+        return $this;
+    }
+
+    public function limit(int | Closure | null $limit = 3): static
+    {
+        $this->limit = $limit;
+
+        return $this;
+    }
+
+    public function getSeparator(): ?string
+    {
+        return $this->evaluate($this->separator);
+    }
+
+    public function getLimit(): ?int
+    {
+        return $this->evaluate($this->limit);
+    }
+
+    public function showRemaining(bool | Closure $condition = true, bool | Closure $showRemainingAsText = false, string | Closure | null $remainingTextSize = null): static
+    {
+        $this->shouldShowRemaining = $condition;
+        $this->showRemainingAsText($showRemainingAsText);
+        $this->remainingTextSize($remainingTextSize);
+
+        return $this;
+    }
+
+    public function showRemainingAsText(bool | Closure $condition = true): static
+    {
+        $this->shouldShowRemainingAsText = $condition;
+
+        return $this;
+    }
+
+    public function shouldShowRemaining(): bool
+    {
+        return $this->evaluate($this->shouldShowRemaining);
+    }
+
+    public function shouldShowRemainingAsText(): bool
+    {
+        return $this->evaluate($this->shouldShowRemainingAsText);
+    }
+
+    public function remainingTextSize(string | Closure | null $remainingTextSize): static
+    {
+        $this->remainingTextSize = $remainingTextSize;
+
+        return $this;
+    }
+
+    public function getRemainingTextSize(): ?string
+    {
+        return $this->evaluate($this->remainingTextSize);
+    }
 }
